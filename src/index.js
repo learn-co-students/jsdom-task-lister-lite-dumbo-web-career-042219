f8d6c74651ef3a04d222222b31d86319ab6b1419
Jasterix <ljcalixte@gmail.com>
2019-06-04 13:39:56 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
+
+
+// As an user, the task string that I provided should appear on the DOM after the submit button has been activated.
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector('#create-task-form');
+  const newTaskDescription = document.querySelector('#new-task-description');
+  const taskDescription = document.getElementById('new-task-description');
+  const taskContainer = document.getElementById('list');
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+     let item = document.createElement("li");
+     item.innerHTML = newTaskDescription.value;
+     taskContainer.appendChild(item);
+     form.reset();
+
+
+
+  })
 });
+
+
+
+  // function removeTask() {
+  //   addEventListene
+  //   removeAttributeNode
+  //   removeEventListener(
+  // }
