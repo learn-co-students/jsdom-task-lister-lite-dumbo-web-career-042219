1cfd3b50c73c0de0f3e6368265f6ea08222584cd
github username <github email address>
2019-06-04 16:20:28 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  button = document.getElementById('create-task-button');
+  button.addEventListener('click', function(e) {
+    e.preventDefault();
+    let list = document.getElementById('tasks');
+    let task = document.getElementById('new-task-description').value;
+    let element = document.createElement("li");
+    element.innerText = task;
+    list.appendChild(element);
+  });
 });
+
+// Stretch Deliverables:
+// A delete function that will remove tasks from your list
+
+  let items = document.getElementById('tasks');
+  items.addEventListener('click', function(e) {
+    for (let itemsLi of items.children) {
+      e.target.remove();
+    };
+  });
